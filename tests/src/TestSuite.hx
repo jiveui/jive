7483adbf6b97a72b9c0422876703e28e95782184
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2015-01-23 12:02:10 +0700

    Fix #37 MVVM Support: JToolBar

--- tests/src/TestSuite.hx
+++ tests/src/TestSuite.hx
@@ -1,6 +1,23 @@
 import massive.munit.TestSuite;
 
+import jive.DataBindingTest;
+import org.aswing.ASColorTest;
+import org.aswing.ASFontTest;
+import org.aswing.AssetBackgroundTest;
+import org.aswing.AssetIconTest;
+import org.aswing.BorderLayoutTest;
+import org.aswing.BordersTest;
+import org.aswing.BoxTest;
+import org.aswing.ComponentTest;
+import org.aswing.FlowLayoutTest;
+import org.aswing.FlowWrapLayoutTest;
+import org.aswing.GridLayoutTest;
+import org.aswing.InsetsTest;
+import org.aswing.JToolBarTest;
 import org.aswing.JToolTipTest;
+import org.aswing.JTreeTest;
+import org.aswing.JWindowTest;
+import org.aswing.SoftBoxTest;
 
 /**
  * Auto generated Test Suite for MassiveUnit.
@@ -14,6 +31,23 @@ class TestSuite extends massive.munit.TestSuite
 	{
 		super();
 
+		add(jive.DataBindingTest);
+		add(org.aswing.ASColorTest);
+		add(org.aswing.ASFontTest);
+		add(org.aswing.AssetBackgroundTest);
+		add(org.aswing.AssetIconTest);
+		add(org.aswing.BorderLayoutTest);
+		add(org.aswing.BordersTest);
+		add(org.aswing.BoxTest);
+		add(org.aswing.ComponentTest);
+		add(org.aswing.FlowLayoutTest);
+		add(org.aswing.FlowWrapLayoutTest);
+		add(org.aswing.GridLayoutTest);
+		add(org.aswing.InsetsTest);
+		add(org.aswing.JToolBarTest);
 		add(org.aswing.JToolTipTest);
+		add(org.aswing.JTreeTest);
+		add(org.aswing.JWindowTest);
+		add(org.aswing.SoftBoxTest);
 	}
 }
