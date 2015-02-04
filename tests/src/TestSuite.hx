b1b428c9c25b9fc0e57b2e887d048b0201238b1a
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2015-02-04 17:03:31 +0700

    - schema changed; - full test suite class.

--- tests/src/TestSuite.hx
+++ tests/src/TestSuite.hx
@@ -1,6 +1,38 @@
 import massive.munit.TestSuite;
 
+import jive.DataBindingTest;
+import org.aswing.ASColorTest;
+import org.aswing.ASFontTest;
+import org.aswing.AssetBackgroundTest;
+import org.aswing.AssetIconTest;
+import org.aswing.BorderLayoutTest;
+import org.aswing.BordersTest;
+import org.aswing.BoxTest;
+import org.aswing.ComponentTest;
+import org.aswing.FlowLayoutTest;
+import org.aswing.FlowWrapLayoutTest;
+import org.aswing.GridLayoutTest;
+import org.aswing.InsetsTest;
+import org.aswing.JAccordionTest;
+import org.aswing.JAdjusterTest;
+import org.aswing.JCheckBoxTest;
+import org.aswing.JComboBoxTest;
+import org.aswing.JLabelButtonTest;
+import org.aswing.JLabelTest;
+import org.aswing.JListTest;
+import org.aswing.JLoadPaneTest;
+import org.aswing.JSeparatorTest;
+import org.aswing.JSharedToolTipTest;
+import org.aswing.JSliderTest;
+import org.aswing.JTabbedPaneTest;
 import org.aswing.JTableTest;
+import org.aswing.JTextAreaTest;
+import org.aswing.JTextFieldTest;
+import org.aswing.JToolBarTest;
+import org.aswing.JToolTipTest;
+import org.aswing.JTreeTest;
+import org.aswing.JWindowTest;
+import org.aswing.SoftBoxTest;
 
 /**
  * Auto generated Test Suite for MassiveUnit.
@@ -14,6 +46,38 @@ class TestSuite extends massive.munit.TestSuite
 	{
 		super();
 
+		add(jive.DataBindingTest);
+		add(org.aswing.ASColorTest);
+		add(org.aswing.ASFontTest);
+		add(org.aswing.AssetBackgroundTest);
+		add(org.aswing.AssetIconTest);
+		add(org.aswing.BorderLayoutTest);
+		add(org.aswing.BordersTest);
+		add(org.aswing.BoxTest);
+		add(org.aswing.ComponentTest);
+		add(org.aswing.FlowLayoutTest);
+		add(org.aswing.FlowWrapLayoutTest);
+		add(org.aswing.GridLayoutTest);
+		add(org.aswing.InsetsTest);
+		add(org.aswing.JAccordionTest);
+		add(org.aswing.JAdjusterTest);
+		add(org.aswing.JCheckBoxTest);
+		add(org.aswing.JComboBoxTest);
+		add(org.aswing.JLabelButtonTest);
+		add(org.aswing.JLabelTest);
+		add(org.aswing.JListTest);
+		add(org.aswing.JLoadPaneTest);
+		add(org.aswing.JSeparatorTest);
+		add(org.aswing.JSharedToolTipTest);
+		add(org.aswing.JSliderTest);
+		add(org.aswing.JTabbedPaneTest);
 		add(org.aswing.JTableTest);
+		add(org.aswing.JTextAreaTest);
+		add(org.aswing.JTextFieldTest);
+		add(org.aswing.JToolBarTest);
+		add(org.aswing.JToolTipTest);
+		add(org.aswing.JTreeTest);
+		add(org.aswing.JWindowTest);
+		add(org.aswing.SoftBoxTest);
 	}
 }
