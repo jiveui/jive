b1cba24a783d5647fa7e9541db08324fc69ad933
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2015-04-22 15:37:37 +0700

    The site is in progress.

--- site/haxe/src/viewmodel/AboutViewModel.hx
+++ site/haxe/src/viewmodel/AboutViewModel.hx
@@ -11,6 +11,9 @@ class AboutViewModel implements IBindable {
     @bindable public var openFlIcon: DisplayObject = Assets.getSvg("openfl.svg");
     @bindable public var brainIcon: DisplayObject = Assets.getSvg("brain.svg");
     @bindable public var desktopIcon: DisplayObject = Assets.getSvg("desktop.svg");
+    @bindable public var arrowIcon(get, set): DisplayObject;
+    private function get_arrowIcon(): DisplayObject { return Assets.getSvg("arrow.svg"); }
+    private function set_arrowIcon(v: DisplayObject): DisplayObject { return v; }
 
     public function new() {
     }
