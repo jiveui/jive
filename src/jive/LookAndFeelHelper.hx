fb456ea30b12e74b8508a91d51cd1f65f8063c73
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2016-03-20 10:23:49 +0700

    Calculate the app dimensions when an app started.

--- src/jive/LookAndFeelHelper.hx
+++ src/jive/LookAndFeelHelper.hx
@@ -3,7 +3,30 @@ package jive;
 import flash.system.Capabilities;
 
 class LookAndFeelHelper {
-    public static var maxDimension = Math.max(Capabilities.screenResolutionX, Capabilities.screenResolutionY);
-    public static var minDimension = Math.min(Capabilities.screenResolutionX, Capabilities.screenResolutionY);
-    public static var scale: Float = Math.floor(Math.min(LookAndFeelHelper.minDimension * 0.350, 0.350 * 2/3 * LookAndFeelHelper.maxDimension))/112;
+    public static var maxDimension(get, null): Float;
+    private static var _maxDimension: Float = -1.0;
+    private static function get_maxDimension(): Float {
+        if (_maxDimension < 0) {
+            _maxDimension = Math.max(Capabilities.screenResolutionX, Capabilities.screenResolutionY);
+        }
+        return _maxDimension;
+    }
+
+    public static var minDimension(get, null): Float;
+    private static var _minDimension: Float = -1.1;
+    private static function get_minDimension(): Float {
+        if (_minDimension < 0) {
+            _minDimension = Math.min(Capabilities.screenResolutionX, Capabilities.screenResolutionY);
+        }
+        return _minDimension;
+    }
+
+    public static var scale(get, null): Float;
+    private static var _scale: Float = -1.1;
+    private static function get_scale(): Float {
+        if (_scale < 0) {
+            _scale = Math.floor(Math.min(LookAndFeelHelper.minDimension * 0.350, 0.350 * 2/3 * LookAndFeelHelper.maxDimension))/112;
+        }
+        return _scale;
+    }
 }
