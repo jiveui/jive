f6b4c97dcc8de00e02045b89256b80c3dcc9ee3a
Alexei Popov <alexei.popov87@gmail.com>
2016-03-21 08:05:01 +0700

    Fixed frame around

--- src/jive/plaf/flat/background/FlatFrameAroundBackground.hx
+++ src/jive/plaf/flat/background/FlatFrameAroundBackground.hx
@@ -1,79 +1,49 @@
-package discount.viewmodel;
+package jive.plaf.flat.background;
 
-import bindx.IBindable;
 
-import openfl.Assets;
-
+import org.aswing.graphics.Pen;
+import org.aswing.AsWingUtils;
+import flash.display.DisplayObject;
+import flash.display.Shape;
+import flash.display.Sprite; 
+import flash.filters.BitmapFilterType; 
+import flash.filters.DropShadowFilter;
+import flash.filters.BitmapFilter;
 import org.aswing.ASColor;
-import org.aswing.ASFont;
-import org.aswing.Insets;
-import org.aswing.AssetBackground;
-import org.aswing.JSpacer;
-import org.aswing.UIManager;
-import org.aswing.geom.IntDimension;
+import org.aswing.Component;
+import org.aswing.GroundDecorator;
+import org.aswing.JFrame;
+import org.aswing.StyleResult;
+import org.aswing.StyleTune;
+import org.aswing.geom.IntRectangle;
+import org.aswing.graphics.Graphics2D;
+import org.aswing.plaf.FrameUI;
+import org.aswing.plaf.UIResource;
+import org.aswing.plaf.basic.BasicGraphicsUtils;
 
-import jive.plaf.flat.background.FlatFrameAroundBackground;
+/**
+ * @private
+ */
+class FlatFrameAroundBackground implements GroundDecorator implements UIResource{
+    
+    private var shape:Sprite;
+    private var color:ASColor;
+    private var thinkness:Float;
 
-import flash.events.Event;
-import flash.display.Loader;
-import flash.net.URLRequest;
-import flash.display.Bitmap;
-
-import common.types.Partner;
-import common.Client;
-
-class SinglePartnerViewModel implements IBindable {
-    @bindable public var partnerName: String;
-    @bindable public var partnerAddress: String;
-    public var partnerPhone: String;
-    @bindable public var comments: String;
-
-    @bindable public var commentsVisible: Bool = false;
-    @bindable public var imageVisible: Bool = false;
-    @bindable public var phoneVisible: Bool = false;
-
-    @bindable public var partnerLogo: AssetBackground;
-    @bindable public var partnerLogoFrame: FlatFrameAroundBackground;
-    @bindable public var mediumFont: ASFont;
-
-    public var partner: Partner;
-
-    public function new(partner: Partner) {
-        this.partner = partner;
-
-        this.partnerName = partner.legalName;
-        this.partnerAddress = if (partner.branches != null && partner.branches.length > 0) partner.branches[0].address else partner.legalAddress;
-        this.partnerPhone = if (partner.branches != null && partner.branches.length > 0) partner.branches[0].phone else partner.phone;
-        this.comments = partner.shortDescription;
-
-        if (this.partnerPhone.length > 0)
-            this.partnerAddress += ", тел.: " + this.partnerPhone;
-
-        var logo: Dynamic = Controller.instance.getLogo(partner);
-        var image: String = if (logo != null) Client.URL + "/" + logo.url else "";
-
-        mediumFont = UIManager.get('mediumControlFont');
-
-        if (partnerPhone.length > 0)
-            phoneVisible = true;
-
-        if (image.length > 0) {
-            var logoLoader = new Loader();
-            logoLoader.contentLoaderInfo.addEventListener(Event.COMPLETE, logoListener);
-            logoLoader.load(new URLRequest(image));
-
-        }
+    public function new(?color: ASColor = null, ?thinkness: Float = 1) {
+        shape = new Sprite();
+        this.color = color == null ? org.aswing.ASColor.BLACK : color;
+        this.thinkness = thinkness;
     }
 
-    private function logoListener(e: Event) {
-        var bitmapLogo: Bitmap = e.target.loader.content;
-        if (bitmapLogo != null) {
-            imageVisible = true;
-            bitmapLogo.width = Std.int(UIManager.get('fontSize') * 4.5 * 4.0);
-            bitmapLogo.height = Std.int(UIManager.get('fontSize') * 4.5 * 4.0);
-
-            partnerLogo = new AssetBackground(bitmapLogo, true);
-            partnerLogoFrame = new FlatFrameAroundBackground();
-        }
+    public function getDisplay(c:Component):DisplayObject{
+        return shape;
     }
+    
+    public function updateDecorator(c:Component, g:Graphics2D, b:IntRectangle):Void{
+        shape.graphics.clear();
+        g = new Graphics2D(shape.graphics);
+        g.drawRoundRect(new Pen(color, thinkness), b.x, b.y, b.width, b.height, c.styleTune.round);
+    }
+    
 }
