ec99670c785b537af5462c50ee56d933c10080bb
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2016-04-02 14:12:11 +0700

    Component.displayObject

--- src/jive/Component.hx
+++ src/jive/Component.hx
@@ -1,10 +1,14 @@
 package jive;
 
+import openfl.display.DisplayObject;
+
 class Component {
     public var x: Metric;
     public var y: Metric;
     public var width: Metric;
     public var height: Metric;
+
+    public var displayObject(default, null): DisplayObject;
 
     public function new() {}
 }
