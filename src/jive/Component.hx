d9ce67c1a27e09b09e2c97fda194a5625fc25a3e
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2016-04-04 12:35:05 +0700

    Revert "Component.displayObject"
    This reverts commit ec99670c785b537af5462c50ee56d933c10080bb.

--- src/jive/Component.hx
+++ src/jive/Component.hx
@@ -1,14 +1,10 @@
 package jive;
 
-import openfl.display.DisplayObject;
-
 class Component {
     public var x: Metric;
     public var y: Metric;
     public var width: Metric;
     public var height: Metric;
-
-    public var displayObject(default, null): DisplayObject;
 
     public function new() {}
 }
