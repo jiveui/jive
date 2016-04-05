f06ec78dec4cb4c6e52478cb7ac4135930e032ed
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2016-04-05 20:52:37 +0700

    - component metrics; - init of TemplatedComponent.

--- src/jive/Component.hx
+++ src/jive/Component.hx
@@ -12,10 +12,36 @@ class Component extends EventDispatcher implements IBindable {
 
     private var needsPaint: Bool;
 
-    public var x: Metric;
-    public var y: Metric;
-    public var width: Metric;
-    public var height: Metric;
+    public var x(get, set): Metric;
+    private var _x: Metric;
+    private function get_x(): Metric { return _x; }
+    private function set_x(v: Metric): Metric {
+        _x = v;
+        return v;
+    }
+    public var y(get, set): Metric;
+    private var _y: Metric;
+    private function get_y(): Metric { return _y; }
+    private function set_y(v: Metric): Metric {
+        _y = v;
+        return v;
+    }
+
+    public var width(get, set): Metric;
+    private var _width: Metric;
+    private function get_width(): Metric { return _width; }
+    private function set_width(v: Metric): Metric {
+        _width = v;
+        return v;
+    }
+
+    public var height(get, set): Metric;
+    private var _height: Metric;
+    private function get_height(): Metric { return _height; }
+    private function set_height(v: Metric): Metric {
+        _height = v;
+        return v;
+    }
 
     public var displayObject(get, null): DisplayObject;
     private function get_displayObject(): DisplayObject {
--- src/jive/TemplatedComponent.hx
+++ src/jive/TemplatedComponent.hx
@@ -1,11 +1,37 @@
 package jive;
 
+import jive.geom.IntDimension;
+
 class TemplatedComponent extends Container {
 
-    public var template: Class<Dynamic>;
-    public var viewModel: ViewModel;
+    private var templateInstance: Component;
+
+    public var template(get, set): Class<Dynamic>;
+    private var _template: Class<Dynamic>;
+    private function get_template(): Class<Dynamic> { return _template; }
+    private function set_template(v: Class<Dynamic>): Class<Dynamic> {
+        _template = v;
+        if (null != templateInstance) { children.remove(templateInstance); }
+        templateInstance = Type.createEmptyInstance(v).as(Component);
+        if (null != viewModel) { templateInstance.dataContext = viewModel; }
+        children.add(templateInstance);
+        return v;
+    }
+
+    public var viewModel(get, set): ViewModel;
+    private var _viewModel: ViewModel;
+    private function get_viewModel(): ViewModel { return _viewModel; }
+    private function set_viewModel(v: ViewModel): ViewModel {
+        _viewModel = v;
+        if (null != templateInstance) templateInstance = viewModel;
+        return v;
+    }
 
     public function new() {
         super();
+    }
+
+    override public function calcSize(): IntDimension {
+        return if (null != templateInstance) templateInstance.calcSize() else new IntDimension(0,0);
     }
 }
