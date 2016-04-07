e33a91dd6319d54d39d1054c55f7b8af8b63836f
klmsmb <maximbekhterev@gmail.com>
2016-04-07 13:20:51 +0700

    Revert "Add Base Layout"
    This reverts commit 168e982314b1b6bcb00b53dd79a36edec3ef88be.

--- src/jive/Container.hx
+++ src/jive/Container.hx
@@ -7,6 +7,7 @@ using jive.geom.MetricHelper;
 
 class Container extends Component {
 
+    private var needsLayout: Bool;
     private var childrenNeedRepaint: Bool;
 
     public var children: Collection<Component>;
@@ -23,7 +24,24 @@ class Container extends Component {
             }
         }
         needsPaint = false;
-        childrenNeedRepaint = false;
+        if (needsLayout) {
+            layout();
+        }
+        needsLayout = false;
+    }
+
+    private function layout() {
+        for (c in children) {
+            c.displayObject.x = c.x.toAbsolute(this);
+            c.displayObject.y = c.y.toAbsolute(this);
+        }
+    }
+
+    public function relayout() {
+        needsLayout = true;
+        if (null != parent) {
+            parent.relayout();
+        }
     }
 
     public function repaintChildren() {
