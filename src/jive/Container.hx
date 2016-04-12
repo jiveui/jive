8e7559b69ecbcbb8634730d1c69ac4b123f5ec6e
klmsmb <maximbekhterev@gmail.com>
2016-04-12 17:44:16 +0700

    work with children

--- src/jive/Container.hx
+++ src/jive/Container.hx
@@ -17,11 +17,30 @@ class Container extends Component {
     public function append(child: Component) {
         children.add(child);
         child.parent = this;
+        child.repaint();
+    }
+
+    public function insert(index: Int, child: Component) {
+        children.add(child, index);
+        child.parent = this;
+        child.repaint();
     }
 
     public function remove(child: Component) {
         children.remove(child);
         child.parent = null;
+
+        repaint();
+    }
+
+    public function removeAll() {
+        for (child in children) {
+            child.parent = null;
+        }
+
+        children.remove();
+
+        repaint();
     }
 
     override public function paint(size: PaintDimension): IntDimension {
