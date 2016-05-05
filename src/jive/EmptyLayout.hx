7a2a369a1b266585ba53273c3fd94420e9ea0a40
klmsmb <maximbekhterev@gmail.com>
2016-05-05 16:20:56 +0700

    work with layouts

--- src/jive/EmptyLayout.hx
+++ src/jive/EmptyLayout.hx
@@ -1,5 +1,7 @@
 package jive;
 
+import openfl.display.DisplayObjectContainer;
+import flash.display.Sprite;
 import jive.geom.Metric;
 import jive.geom.IntDimension;
 import jive.geom.IntDimension;
@@ -13,6 +15,78 @@ class EmptyLayout extends Container {
         super();
 
         needsLayout = false;
+    }
+
+    override private function get_displayObjectContainer(): Sprite {
+        if (null == displayObjectContainer && null != parent) {
+            displayObjectContainer = parent.displayObjectContainer;
+            displayObject = displayObjectContainer;
+        }
+        return displayObjectContainer;
+    }
+
+    override private function createDisplayObjectContainer(): Sprite {
+        return null;
+    }
+
+    private function rebase() {
+        for (child in children) {
+            if (child.displayObject != null && child.displayObject.parent != null)
+                child.displayObject.parent.removeChild(child.displayObject);
+        }
+
+        if (displayObjectContainer != null) {
+            for (child in children) {
+                displayObjectContainer.addChild(child.displayObject);
+            }
+        }
+
+        for (child in children) {
+            if (Std.is(child, EmptyLayout)) {
+                cast(child, EmptyLayout).rebase();
+            }
+        }
+
+        relayout();
+    }
+
+    override private function set_parent(c: Container): Container {
+        var needRebase: Bool =  parent != c;
+        parent = c;
+        if (needRebase)
+            rebase();
+
+        return parent;
+    }
+
+    override public function append(child: Component) {
+        children.add(child);
+        child.parent = this;
+
+        if (displayObjectContainer != null) {
+            displayObjectContainer.addChild(child.displayObject);
+            child.repaint();
+        }
+    }
+
+    override public function insert(index: Int, child: Component) {
+        children.add(child, index);
+        child.parent = this;
+
+        if (displayObjectContainer != null) {
+            displayObjectContainer.addChildAt(child.displayObject, index);
+            child.repaint();
+        }
+    }
+
+    override public function remove(child: Component) {
+        if (displayObjectContainer != null) {
+            displayObjectContainer.removeChild(child.displayObject);
+            repaint();
+        }
+
+        children.remove(child);
+        child.parent = null;
     }
 
     override private function calcPaintDimension(size: MetricDimension): MetricDimension {
--- src/jive/Flow.hx
+++ src/jive/Flow.hx
@@ -10,29 +10,16 @@ class Flow extends EmptyLayout {
         super();
     }
 
-/*    override private function calcPaintDimension(size: MetricDimension): MetricDimension {
-        return new MetricDimension(_width, _height);
-    }
-
-    override public function paint(size: MetricDimension): IntDimension {
-        for (child in children) {
-            var childSize: IntDimension = child.dimension;
-            if (!child.paint(calcPaintDimension(size)).equals(childSize)) {
-                needsLayout = true;
-            }
-        }
-
-        layout();
-
-        return IntDimension.createNullDimension();
-    }*/
-
     override private function layout() {
         if (needsLayout) {
             needsLayout = false;
+            var x: Float = 0;
+            var y: Float = 0;
             for (child in children) {
-                child.displayObject.x = child.absoluteX;
-                child.displayObject.y = child.absoluteY;
+                child.displayObject.x = x;
+                child.displayObject.y = y;
+                x+=child.absoluteWidth;
+                //y+=child.absoluteHeight;
             }
         }
     }
