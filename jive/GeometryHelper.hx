543bc96697ed65c4c0619dc84d1ef7700c810101
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2018-06-21 09:38:29 +0700

    GeometryHelper added

---
+++ jive/GeometryHelper.hx
@@ -0,0 +1,14 @@
+package jive;
+
+import openfl.geom.Matrix;
+class GeometryHelper {
+    public static function createRotationMatric(angle: Float, pivotX: Float, pivotY: Float) {
+        var matrix:Matrix = new Matrix();
+        if (angle != 0.0) {
+            matrix.translate(-pivotX, -pivotY);
+            matrix.rotate(angle / 180 * Math.PI);
+            matrix.translate(pivotX, pivotY);
+        }
+        return matrix;
+    }
+}
